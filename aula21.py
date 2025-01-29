d327afa0c99153fe2c2821c2ec6f51b82b50d99a
AnolinoJunior <anolinojunioralves@gmail.com>
2025-01-29 17:36:54 -0300

    python
    exercicios novos

--- aula21.py
+++ aula21.py
@@ -8,3 +8,20 @@
 # 0.0.0 '' False
 #Também existe o tipo None que é 
 #usado para representar um não valor
+
+'''entrada = input('[E]ntrar [S]air:')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '123456'
+
+#if só é executada quando a condição for TRUE
+
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair') '''
+
+#Avaliação de curto circuito
+#print(True and True and True)
+senha = input('senha: ') or 'Sem senha'
+print(senha)
---
+++ aula22.py
@@ -0,0 +1,6 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = false
+# not true = true
+print(not True) # False
+print(not False) #True
