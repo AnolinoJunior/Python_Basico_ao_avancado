62ea51ac86be0f948dd0c626b0249e73ab4cf4b3
AnolinoJunior <anolinojunioralves@gmail.com>
2025-02-03 21:32:54 -0300

    create aula28.py

---
+++ aula28.py
@@ -0,0 +1,25 @@
+'''
+introduçao ao try/except
+try -> tentar executar o codigo
+except -> ocorreu algum erro ao tentar executar
+'''
+numero_str = input ('Vou Dobrar o numero que voce digitar:')
+
+try:
+    print('STR:', numero_str)
+    numero_float = float{numero_str}
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numero_str} eh  {numero_float * 2:.0f}')
+
+except:
+    print('Isso nao eh um numero')
+    
+
+'''
+if numero_str.isdigit():
+
+    numero_float = float{numero_str}
+    print(f'O dobro de {numero_str} eh  {numero_float * 2:.0f}')
+ else:
+     print('isso nao eh um numero inteiro')
+     '''
