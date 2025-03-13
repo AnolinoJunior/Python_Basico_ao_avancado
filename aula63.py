baa0709b1f624b456bb03f910d50114a54b1dfae
AnolinoJunior <anolinojunioralves@gmail.com>
2025-03-13 16:19:18 -0300

    subir aulas
    aulas

---
+++ aula63.py
@@ -0,0 +1,18 @@
+"""
+INTRODUÇÃO AS FUNÇOES (def) em python
+funções são trechos de código usados para
+replicar determinada ação ao longo do seu código.
+Elas podem receber valores para parâmetros (argumentos)
+e retornar um valor específico.
+Por padrão, funções Python retornam None (nada).    
+"""
+
+def saudacao(nome='sem nome'):
+    print(f'Olá, {nome}!')
+
+
+
+saudacao('Anolino Alves')
+saudacao('Marina Catuaba')
+saudacao('Eny de Oliveira')
+saudacao()
