113f60d593bc3b0877f8efae6e69ccd9de5799a8
AnolinoJunior <anolinojunioralves@gmail.com>
2025-03-14 08:50:41 -0300

    subir minhas aulas
    aulas

--- aula69.py
+++ aula69.py
@@ -21,8 +21,8 @@ soma_1_2_3 = soma(1,2,3)
 print(soma_1_2_3)
 
 soma_4_5_6 = soma(4,5,6)
-#print(soma_4_5_6)
-numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+print(soma_4_5_6)
+numeros = 1, 2, 3, 4, 5, 6, 7, 8, 9
 outra_soma = soma (*numeros)
 print(outra_soma)
 
